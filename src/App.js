dfc8d16817a87181b312ad8d339b85b10be4d09f
sun4205 <hanna4205@gmail.com>
2025-07-26 13:32:35 -0400

    update bookcreate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
 function App() {
-  return <div>App</div>;
+  const [book, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log("need to add", title);
+  };
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
