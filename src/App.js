aba3923a0d9222ab1609e169fcc1db61be99749d
sun4205 <hanna4205@gmail.com>
2025-08-07 14:11:53 -0400

    add axios edit and delete function

--- src/App.js
+++ src/App.js
@@ -22,14 +22,15 @@ function App() {
     console.log(response);
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
       return book;
     });
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
